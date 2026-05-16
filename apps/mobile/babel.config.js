16b137a37a7c3864a43eb2593b83a4beb68fc508
yasithnirmal88 <synpeiris@gmail.com>
2026-05-16 22:47:33 +0530

    Diagnostic: Removed NativeWind from Babel config to find the source of the invalid plugin property error.

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,10 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "babel-preset-expo",
     ],
     plugins: [
-      "nativewind/babel",
       "react-native-reanimated/plugin",
     ],
   };

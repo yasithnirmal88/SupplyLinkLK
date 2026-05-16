dda07512a97835e21b73375eb362ca7dae9dfea4
yasithnirmal88 <synpeiris@gmail.com>
2026-05-16 22:47:33 +0530

    Fix: Configured NativeWind as a preset in Babel to resolve validation error.

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -3,9 +3,9 @@ module.exports = function (api) {
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
     plugins: [
-      "nativewind/babel",
       "react-native-reanimated/plugin",
     ],
   };
